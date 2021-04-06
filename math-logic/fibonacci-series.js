7b6f0727065c9812881a4e799dc93dbbc3380a80
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-04-06 17:17:31 -0500

    Memoization implementation

--- math-logic/fibonacci-series.js
+++ math-logic/fibonacci-series.js
@@ -1,12 +1,15 @@
 class FibonacciSeries {
   constructor() {}
-  calculateFibonacciValue(number) {
+  calculateFibonacciValue(number, memo) {
+    memo = memo || {};
+
+    if (memo[number]) return memo[number];
     if (number === 0) return 0;
     if (number === 1) return 1;
 
-    return (
-      this.calculateFibonacciValue(number - 1) +
-      this.calculateFibonacciValue(number - 2)
+    return memo[number] = (
+      this.calculateFibonacciValue(number - 1, memo) +
+      this.calculateFibonacciValue(number - 2, memo)
     );
   }
 }
